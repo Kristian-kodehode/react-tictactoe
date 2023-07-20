9576df4a419a0b2a0584d9c5332d19591a304baa
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-07-20 12:45:36 +0200

    init commit

---
+++ src/Square.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Board from "./Board";
+
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
