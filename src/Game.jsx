cda0fc789d6e0016f4b76e68a81580cd29f21f6a
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-07-20 18:20:46 +0200

    modded Game comp to render current selected move

--- src/Game.jsx
+++ src/Game.jsx
@@ -5,7 +5,7 @@ const Game = () => {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
