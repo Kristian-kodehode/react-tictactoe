d5a0a9ae5e948733f804249b0c1b89026a9717ba
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-07-20 14:57:12 +0200

    Added font family and cleaned up css a bit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,18 @@ import Board from "./Board";
 const App = () => {
   return (
     <>
+      <section>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </section>
       <div>
         <div className="wave"></div>
         <div className="wave"></div>
