7a7f5e5f6b87d4f5ec0e7f1ad1e2f805d3486409
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-07-21 13:10:35 +0200

    Adjusted the reset game function

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from "react";
 import Board from "./Board";
 
@@ -22,6 +23,39 @@ const Game = () => {
       </div>
       <div className="game-info">
         <button onClick={() => jumpTo(0)}>Reset Game</button>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
+*/
+
+import { useState } from "react";
+import Board from "./Board";
+
+const Game = () => {
+  const [currentSquares, setCurrentSquares] = useState(Array(9).fill(null));
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+
+  const handlePlay = (nextSquares) => {
+    setCurrentSquares(nextSquares);
+    setCurrentMove(currentMove + 1);
+  };
+
+  const resetGame = () => {
+    setCurrentSquares(Array(9).fill(null));
+    setCurrentMove(0);
+  };
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <button onClick={resetGame}>Reset Game</button>
       </div>
     </div>
   );
